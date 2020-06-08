8f5457ef27c05651fa6c17e7d05637e5d65d30f8
Katja Ruebel <Katja.Ruebel@gmail.com>
2020-06-09 00:16:32 +0200

    changed size

--- relaxer-app/script.js
+++ relaxer-app/script.js
@@ -2,8 +2,7 @@ const container = document.getElementById('container');
 const text = document.getElementById('text');
 
 const totalTime = 12000;
-const breatheInTime = (totalTime / 12) * 5;
-const breatheOutTime = (totalTime / 12) * 6;
+const breatheTime = (totalTime / 12) * 5;
 const holdTime = (totalTime / 12) * 1;
 
 breathAnimation();
@@ -19,7 +18,7 @@ function breathAnimation() {
       text.innerText = 'Breathe Out!';
       container.className = 'container shrink';
     }, holdTime);
-  }, breatheInTime);
+  }, breatheTime);
 }
 
 setInterval(breathAnimation, totalTime);
